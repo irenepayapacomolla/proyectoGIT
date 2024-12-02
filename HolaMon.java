eb2a61d8ae88228dee328b49f69e4ac067feddb9
irenepayapacomolla <irenepaya.alu@iespacomolla.es>
2024-12-02 16:14:35 +0100

    ahora es Hola git

--- HolaMon.java
+++ HolaMon.java
@@ -9,7 +9,7 @@ public class HolaMon {
 
    
     public static void main(String[] args) {
-        System.out.println("HOLA MON !!!!");
+        System.out.println("Hola GIT !!!!");
     }
     
 }
